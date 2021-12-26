a01b121dcdf1da95c99965079b758bd374260296
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-12-26 22:50:58 +0000

    Remove: previous project startup with nodejs

--- src/routes/health-check.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const router = Router();
-
-router.get("/", function(req, res) {
-    res.send("hello world!");
-});
-
-module.exports = router;
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const faculties = require("./faculties");
-const example = require("./health-check");
-
-router.use("/faculties", faculties);
-router.use("/health-check", example);
-
-
-module.exports = router;
