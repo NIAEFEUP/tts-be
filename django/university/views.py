0a87d9e3595041e5e7fae3623505e00aaade207e
Sergio Estevao <smestevao11@gmail.com>
2022-01-08 14:48:28 +0000

    add: course view

--- django/university/views.py
+++ django/university/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
 from university.models import Faculty
+from university.models import Course
 from django.http import JsonResponse
 from django.core import serializers
 from rest_framework.decorators import api_view
@@ -9,4 +10,9 @@ import json
 @api_view(['GET'])
 def faculty(request): 
     json_data = serializers.serialize('json', Faculty.objects.all())
+    return HttpResponse(json_data, content_type="application/json")
+
+@api_view(['GET'])
+def course(request):
+    json_data = serializers.serialize('json', Course.objects.all())
     return HttpResponse(json_data, content_type="application/json")
