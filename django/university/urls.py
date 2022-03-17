81e32a3cc8b522b0a194eb5ba1ba5672c9b34246
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-03-17 17:57:49 +0000

    Add: try to group course units of the same year. Error...

--- django/university/urls.py
+++ django/university/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('faculty/', views.faculty),
     path('course/', views.course),
-    path('course_units/<int:course_id>/<int:semester>/', views.course_units),
+    path('course_units/<int:course_id>/<int:semester>/', views.course_units), 
+    path('course_units_by_year/<int:course_id>/<int:semester>/', views.course_units_by_year),
     path('schedule/<int:course_unit_id>/', views.schedule)
 ]
+    
--- django/university/views.py
+++ django/university/views.py
@@ -25,6 +25,25 @@ def course_units(request, course_id, semester):
     json_data = serializers.serialize('json', CourseUnit.objects.filter(course=course_id, semester=semester))
     return HttpResponse(json_data, content_type="application/json")
 
+@api_view(['GET'])
 def schedule(request, course_unit_id):
     json_data = serializers.serialize('json', Schedule.objects.filter(course_unit=course_unit_id))
     return HttpResponse(json_data, content_type="application/json")
+
+
+def course_units_by_year(request, course_id, semester): 
+    query_result = list(CourseUnit.objects.filter(course=course_id, semester=semester).order_by('course_year'))  
+    number_of_years = query_result[-1].course_year
+
+    res = []
+    for _ in range(number_of_years): 
+        res.append([])
+
+    print(res)
+    for course_unit in query_result: 
+        res[course_unit.course_year-1].append(course_unit) 
+
+    print(res)
+    json_data = serializers.serialize('json', res)   
+    return HttpResponse(json_data, content_type="application/json")
+
