7201830271afe32768b8ebf831841bef07bdeeb0
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-04-03 14:56:15 +0100

    Add: request last course year and specific units from course

--- django/university/views.py
+++ django/university/views.py
@@ -6,6 +6,8 @@ from university.models import Schedule
 from django.http import JsonResponse
 from django.core import serializers
 from rest_framework.decorators import api_view
+from django.db.models import Max
+
 import json
 # Create your views here. 
 
@@ -16,34 +18,38 @@ def faculty(request):
 
 @api_view(['GET'])
 def course(request):
-    json_data = serializers.serialize('json', Course.objects.all())
-    return HttpResponse(json_data, content_type="application/json")
+    data_json = serializers.serialize('json', Course.objects.all()) 
+    return HttpResponse(data_json, content_type="application/json")
 
-
+"""
+    Return all the units from a course. 
+"""
 @api_view(['GET'])
-def course_units(request, course_id, semester):
-    json_data = serializers.serialize('json', CourseUnit.objects.filter(course=course_id, semester=semester))
+def course_units(request, course_id, semester): 
+    json_data = serializers.serialize('json', CourseUnit.objects.filter(course=course_id, semester=semester).order_by('year')) 
     return HttpResponse(json_data, content_type="application/json")
+
+"""
+    Returns the last year of a course.
+"""
+@api_view(['GET'])
+def course_last_year(request, course_id):
+    max_year = CourseUnit.objects.filter(course=course_id).aggregate(Max('course_year')).get('course_year__max')
+    json_data = {"max_year": max_year}
+    return JsonResponse(json_data)
+
+
+"""
+    Get all the units of a course in a certain year. 
+"""
+@api_view(['GET'])
+def course_units_by_year(request, course_id, year, semester): 
+    json_data = serializers.serialize(CourseUnit.objects.filter(course=course_id, semester=semester, year=year)) 
+    return HttpResponse(json_data, content_type="application/json")
+
 
 @api_view(['GET'])
 def schedule(request, course_unit_id):
     json_data = serializers.serialize('json', Schedule.objects.filter(course_unit=course_unit_id))
-    return HttpResponse(json_data, content_type="application/json")
-
-
-def course_units_by_year(request, course_id, semester): 
-    query_result = list(CourseUnit.objects.filter(course=course_id, semester=semester).order_by('course_year'))  
-    number_of_years = query_result[-1].course_year
-
-    res = []
-    for _ in range(number_of_years): 
-        res.append([])
-
-    print(res)
-    for course_unit in query_result: 
-        res[course_unit.course_year-1].append(course_unit) 
-
-    print(res)
-    json_data = serializers.serialize('json', res)   
     return HttpResponse(json_data, content_type="application/json")
 
