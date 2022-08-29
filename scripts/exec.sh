ebad659eada15734a7d5c4e7c35724555c104ffe
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-08-29 13:44:19 +0100

    update: remove tts_bridge network

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,20 +1,3 @@
-#!/usr/bin/env bash 
-
-if [ -f "./mysql/data/" ] ; then
-    sudo rm -rf "./mysql/data/"
-    echo ">> ./mysql/data/ deleted with success" 
-fi
-
-DOCKER_NETWORKS=$(docker network ls)
-TTS_NETWORK=$(echo $DOCKER_NETWORKS | grep 'tts_bridge ')
-if [ -z "$TTS_NETWORK" ]
-then 
-    echo ">> Missing tts_bridge network. Creating..."
-    docker network create 'tts_bridge'
-else 
-    echo ">> Detected tts_bridge network!"
-fi
-
 echo ">> Formating ./django/entrypoint.sh file"
 sed -i 's/\r$//' ./django/entrypoint.sh   
 
