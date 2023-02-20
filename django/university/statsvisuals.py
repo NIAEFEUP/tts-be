23257ef97c5097ff2b6d923a61f56086327b89cd
Wallen Ribeiro <up202109260@g.uporto.pt>
2023-02-20 12:25:29 +0000

    created the chart creator functions

--- django/university/statsvisuals.py
+++ django/university/statsvisuals.py
@@ -1,6 +1,24 @@
 import json
 import matplotlib.pyplot as plt
 
+def line_chart(keys, values):
+    plt.plot(keys, values)
+    plt.title("Analysis Tool Usage - Line Chart")
+    plt.xlabel("Courses")
+    plt.ylabel("Frequency")
+    plt.show()
+
+def bar_chart(keys, values):
+    plt.bar(keys, values)
+    plt.title("Analysis Tool Usage - Bar Chart")
+    plt.xlabel("Courses")
+    plt.ylabel("Frequency")
+    plt.show()
+
+def pie_chart(keys, values):
+    plt.pie(values, labels=keys, autopct='%1.1f%%')
+    plt.title("Analysis Tool Usage - Pie Chart")
+    plt.show()
 
 with open('./cache/statistics_cache.json') as file:
     stats = json.load(file)
@@ -14,4 +32,5 @@ values = list(stats.values())
 line_chart(keys, values)
 bar_chart(keys, values)
 pie_chart(keys, values)
+
 
