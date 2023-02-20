bab5618b2718cd41d5d566187d3c86417fd3841f
Wallen Ribeiro <up202109260@g.uporto.pt>
2023-02-20 12:23:22 +0000

    defined signatures for the chart creator functions

--- django/university/statsvisuals.py
+++ django/university/statsvisuals.py
@@ -1,3 +1,17 @@
 import json
 import matplotlib.pyplot as plt
 
+
+with open('./cache/statistics_cache.json') as file:
+    stats = json.load(file)
+
+stats = {k: v for k, v in stats.items() if v > 0}
+
+keys = list(stats.keys())
+values = list(stats.values())
+
+# Show Graphs
+line_chart(keys, values)
+bar_chart(keys, values)
+pie_chart(keys, values)
+
