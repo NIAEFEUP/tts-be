b342004f56db57e9861807e35f5add004fa820e3
joaonevesf <joaodnfernandes99@gmail.com>
2024-06-06 17:33:58 +0100

    fix: ocorr_id already has class and schedule, last pr inserted this bug
    TODO: change logic on update_schedule so it doesnt use unit names

--- django/university/exchange/utils.py
+++ django/university/exchange/utils.py
@@ -247,8 +247,8 @@ def update_schedule(student_schedule, exchanges, cookies):
 
     for exchange in exchanges:
         for i, schedule in enumerate(student_schedule):
-            ocorr_id = schedule["ocorrencia_id"]
-            if ocorr_id == exchange.course_unit_id:
+            if schedule["ucurr_sigla"] == exchange.course_unit:
+                ocorr_id = schedule["ocorrencia_id"]
                 class_type = schedule["tipo"]
 
                 unit_schedules = requests.get(get_unit_schedule_url(
