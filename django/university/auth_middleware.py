0ce77b5b91703f8e7a4eb33f9b00262d0d89aceb
João Fernandes <93282636+joaonevesf@users.noreply.github.com>
2024-06-06 13:01:10 +0100

    fixed bug in middleware (#34)

--- django/university/auth_middleware.py
+++ django/university/auth_middleware.py
@@ -1,29 +1,40 @@
 import re
+
 from django.http import HttpResponseForbidden
 
 class AuthMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.auth_paths = [
-            'logout/',
+            '/logout/',
             re.compile(r'^/student_schedule/\d+/$'),
             re.compile(r'^/schedule_sigarra/\d+/$'),
             re.compile(r'^/class_sigarra_schedule/\d+/.+/$'),
-            'submit_direct_exchange/',
+            '/submit_direct_exchange/',
             re.compile(r'^/verify_direct_exchange/.+/$'),
             re.compile(r'^/students_per_course_unit/\d+/$'),
-            'is_admin/',
-            'export/',
-            'direct_exchange/history/',
+            '/is_admin/',
+            '/export/',
+            '/direct_exchange/history/',
         ]
 
     def __call__(self, request):
 
-        if not request.path in self.auth_paths:
+        in_paths = False
+
+        for path in self.auth_paths:
+            if isinstance(path, str) and request.path == path:
+                in_paths = True
+                break
+            elif isinstance(path, re.Pattern) and path.match(request.path):
+                in_paths = True
+                break
+
+        if not in_paths:
             return self.get_response(request)
 
-        if not request.user.is_authenticated:
-            return HttpResponseForbidden
+        if not request.session["username"]:
+            return HttpResponseForbidden()
 
         return self.get_response(request)
 
