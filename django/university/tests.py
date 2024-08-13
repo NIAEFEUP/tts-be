d3224ae453f87ed0adaaa9423489cb18b25dc9de
jose-carlos-sousa <139002032+jose-carlos-sousa@users.noreply.github.com>
2024-08-13 18:24:10 +0100

    fix: removed tests

--- django/university/tests.py
+++ django/university/tests.py
@@ -2,12 +2,3 @@ from django.test import TestCase
 
 # Create your tests here.
 print("olá")
-
-
-#teste de hash
-print("A hash está correta???")
-hash_check=True
-if hash_check:
-    print(" PASS")
-else:
-    print(" FAIL")
