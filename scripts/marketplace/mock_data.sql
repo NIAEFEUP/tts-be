98d0ef44069e0e11a54da3cda1141035a388a815
Tomás Palma <up202108880@edu.fe.up.pt>
2024-09-19 11:31:18 +0100

    chore: add scripts to populate marketplace with mock data

---
+++ scripts/marketplace/mock_data.sql
@@ -0,0 +1,28 @@
+DELETE FROM marketplace_exchange;
+DELETE FROM marketplace_exchange_class;
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (1, 'Armindo Santos', '202108880', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (1, 'Aprendizagem Computacional', 'AC', 540676, '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (2, 'António Santos', '202108881', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (2, 'Aprendizagem Computacional', 'AC', 540676 , '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (3, 'Honda Santos', '202108882', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (3, 'Aprendizagem Computacional', 'AC', 540676 , '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (4, 'João Horácio', '202108883', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (4, 'Aprendizagem Computacional', 'AC', 540676, '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (5, 'Óscar Cardoso', '202108884', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (5, 'Aprendizagem Computacional', 'AC', 540676, '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (6, 'Ana Oliveira', '202108885', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (6, 'Aprendizagem Computacional', 'AC',540676 , '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (7, 'Marco João', '202108886', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (7, 'Aprendizagem Computacional', 'AC',540676 , '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (8, 'Respect Freedom', '202108178', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (8, 'Aprendizagem Computacional', 'AC',540676 , '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (9, 'Faria Algo', '202108771', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (9, 'Aprendizagem Computacional', 'AC', 540676, '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (10, 'André Peixoto', '202108891', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (10, 'Aprendizagem Computacional', 'AC', 540676, '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (11, 'André Paixão', '202108889', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (11, 'Aprendizagem Computacional', 'AC',540676 , '1MEIC01', '1MEIC02');
+INSERT INTO marketplace_exchange (id, issuer_name, issuer_nmec, accepted) VALUES (12, 'Gaspar João', '202108874', false);
+INSERT INTO marketplace_exchange_class (marketplace_exchange, course_unit_name, course_unit_acronym, course_unit_id, class_issuer_goes_from, class_issuer_goes_to) VALUES (12, 'Aprendizagem Computacional', 'AC',540676 , '1MEIC01', '1MEIC02');
+
+
