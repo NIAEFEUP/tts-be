ee41ba884485719ae8397bf5e040b27131d55397
Process-ing <rbruno.p.o@gmail.com>
2024-10-01 17:49:36 +0100

    Fix broken imports

--- django/tts_be/asgi.py
+++ django/tts_be/asgi.py
@@ -8,11 +8,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from university.routing import websocket_urlpatterns
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 
 import socketio
 from university.socket.views import sio
