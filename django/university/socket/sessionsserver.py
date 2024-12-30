c26a1460ec3cbf98be2cf0a110ac1a6c1eec23d7
Process-ing <rbruno.p.o@gmail.com>
2024-12-30 11:41:54 +0000

    Create session endpoints
    Co-authored-by: Rodrigo Coelho <racoelhosilva@users.noreply.github.com>

--- django/university/socket/sessionsserver.py
+++ django/university/socket/sessionsserver.py
@@ -1,11 +1,50 @@
-import uuid
+from typing import Coroutine
+from random import randbytes
+import socketio
 
 class SessionsServer:
-    def __init__(self, sio):
+    def __init__(self, sio: socketio.AsyncServer):
         self.sio = sio
 
-    def valid_token(self, token):
+    def valid_token(self, token: str) -> bool:
         return True
         
     def event(self, event):
         return self.sio.event(event)
+    
+    def emit(self, event, data, **kwargs):
+        return self.sio.emit(event, data, **kwargs)
+    
+    def generate_room_id(self):
+        while True:
+            room_id = randbytes(2).hex().upper()
+            if room_id not in self.rooms:
+                return room_id
+    
+    def create_room(self, sid) -> Coroutine:        
+        if self.get_client_room(sid):
+            raise ConnectionError('Client is already in a room')
+        
+        room_id = self.generate_room_id()
+        return self.sio.enter_room(sid, room_id)
+    
+    def enter_room(self, sid, room_id) -> Coroutine:
+        if self.get_client_room(sid):
+            raise ConnectionError('Client is already in a room')
+        
+        if not self.sio.manager.rooms[room_id]:
+            raise ConnectionError('Room is empty')
+        
+        return self.sio.enter_room(sid, room_id)
+    
+    def leave_room(self, sid) -> Coroutine:
+        room_id = self.get_client_room(sid)
+        return self.sio.leave_room(sid, room_id)
+
+    @property
+    def rooms(self):
+        return self.sio.manager.rooms.keys()
+    
+    def get_client_room(self, sid):
+        client_room = self.sio.rooms(sid)
+        return client_room[1] if len(client_room) >= 2 else None
--- django/university/socket/views.py
+++ django/university/socket/views.py
@@ -1,33 +1,51 @@
 import os
 import socketio
+from urllib.parse import parse_qs
 
 from university.socket.sessionsserver import SessionsServer
 
 async_mode = None
 
 basedir = os.path.dirname(os.path.realpath(__file__))
-sio = socketio.AsyncServer(
-    async_mode='asgi',
-    cors_allowed_origins="*",
+
+sessions_server = SessionsServer(
+    socketio.AsyncServer(
+        async_mode='asgi',
+        cors_allowed_origins="*",
+    )
 )
 
-session_server = SessionsServer(sio)
-        
-@session_server.event
+@sessions_server.event
 async def connect(sid, environ, auth):
     if auth is None or 'token' not in auth:
         raise ConnectionRefusedError('Authentication failed: No token provided')
-    elif not session_server.valid_token(auth['token']):
+    if not sessions_server.valid_token(auth['token']):
         raise ConnectionRefusedError('Authentication failed: Invalid token')
+    
+    print('Client connected')
+    
+    query_params = parse_qs(environ.get("QUERY_STRING", ""))
+    room_id = query_params.get('room_id', None)
+    room_id = room_id[0] if room_id else None
+    
+    if room_id is None:
+        await sessions_server.create_room(sid)
+    
+        room_id = sessions_server.get_client_room(sid)
+        print(f"Room created: {room_id}")
     else:
-        print('Client connected')
-        await sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
+        await sessions_server.enter_room(sid, room_id)
+        print(f"Client {sid} joined room {room_id}")
+    
+    await sessions_server.emit('connected', { 'room' : room_id }, to=sid)
 
 
-@session_server.event
-def disconnect(sid):
+@sessions_server.event
+async def disconnect(sid):
+    await sessions_server.leave_room(sid)
     print('Client disconnected')
     
-@session_server.event
+@sessions_server.event
 async def test(sid, environ):
-    await sio.emit('response', 'ya')
+    await sessions_server.sio.disconnect(sid)
+    
