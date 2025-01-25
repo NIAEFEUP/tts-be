3141a4deb51d9acd6d73d0419f17edde0be6a0b2
Tomás Palma <up202108880@edu.fe.up.pt>
2025-01-25 15:03:28 +0000

    feat: return error if student is already enrolled in course unit

--- django/university/routes/course_unit/CourseUnitEnrollmentView.py
+++ django/university/routes/course_unit/CourseUnitEnrollmentView.py
@@ -1,16 +1,21 @@
 import json
 from rest_framework.views import APIView
-from university.models import CourseUnitEnrollments
+from university.models import CourseUnitEnrollments, UserCourseUnits
 
-from django.http import HttpResponse 
+from django.http import HttpResponse, JsonResponse
 
 class CourseUnitEnrollmentView(APIView):
     def post(self, request):
         enrollments = request.POST.getlist("enrollCourses[]")
 
+        student_course_units = list(UserCourseUnits.objects.filter(user_nmec=request.user.username).all())
+
         models_to_save = []
         for enrollment in enrollments:
-            enrollment_metadata = json.loads(enrollment)
+            enrollment_metadata = json.loads(enrollment) 
+
+            if len(list(filter(lambda x: x.course_unit_id == enrollment_metadata["course_unit_id"], student_course_units))) > 0:
+                return JsonResponse({"error": "Não te podes inscrever a disciplinas em que já tens uma inscrição!"}, status=400)
 
             db_enrollment = CourseUnitEnrollments(
                 user_nmec=request.user.username,
