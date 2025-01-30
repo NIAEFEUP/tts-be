a5ad3bf8fa7f8142a5bd66155cca227498186946
Process-ing <rbruno.p.o@gmail.com>
2025-01-30 08:13:44 +0000

    Store multiple options in session

--- django/university/socket/session.py
+++ django/university/socket/session.py
@@ -7,6 +7,7 @@ class Session:
         self.session_id = session_id
         self.participants = {}
         self.picked_courses = []
+        self.multiple_options = [{"id":0,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0031-fe0f-20e3.png","name":"Horário 1","course_options":[],"chosen":False,"selected":False},{"id":1,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0032-fe0f-20e3.png","name":"Horário 2","course_options":[],"chosen":False,"selected":False},{"id":2,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0033-fe0f-20e3.png","name":"Horário 3","course_options":[],"chosen":False,"selected":False},{"id":3,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0034-fe0f-20e3.png","name":"Horário 4","course_options":[],"chosen":False,"selected":False},{"id":4,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0035-fe0f-20e3.png","name":"Horário 5","course_options":[],"chosen":False,"selected":False},{"id":5,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0036-fe0f-20e3.png","name":"Horário 6","course_options":[],"chosen":False,"selected":False},{"id":6,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0037-fe0f-20e3.png","name":"Horário 7","course_options":[],"chosen":False,"selected":False},{"id":7,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0038-fe0f-20e3.png","name":"Horário 8","course_options":[],"chosen":False,"selected":False},{"id":8,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/0039-fe0f-20e3.png","name":"Horário 9","course_options":[],"chosen":False,"selected":False},{"id":9,"icon":"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/1f51f.png","name":"Horário 10","course_options":[],"chosen":False,"selected":False}]
         self.expiration_datetime = datetime.now(timezone.utc) + duration
         
     def add_client(self, participant: Participant):
@@ -28,6 +29,7 @@ class Session:
         return {
             'expiration_time': int(time.mktime(self.expiration_datetime.timetuple())) * 1000.0,
             'picked_courses': self.picked_courses,
+            'multiple_options': self.multiple_options,
             'participants': list(map(Participant.to_json, self.participants.values()))
         }
     
