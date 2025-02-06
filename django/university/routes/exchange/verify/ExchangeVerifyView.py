42b8f1aea812c68a97a12d05c88a1d0e9ccce378
Tomás Palma <up202108880@edu.fe.up.pt>
2025-02-06 00:23:33 +0000

    fix: if user already verified, the backends returns verified as True still

--- django/university/routes/exchange/verify/ExchangeVerifyView.py
+++ django/university/routes/exchange/verify/ExchangeVerifyView.py
@@ -21,13 +21,19 @@ class ExchangeVerifyView(View):
 
             direct_exchange = DirectExchange.objects.get(id=exchange_info["exchange_id"])
 
+            if(
+                len(DirectExchangeParticipants.objects.filter(direct_exchange=direct_exchange, participant_nmec=request.user.username, accepted=True)) ==
+                len(DirectExchangeParticipants.objects.filter(direct_exchange=direct_exchange, participant_nmec=request.user.username))
+            ):
+                return JsonResponse({"verified": True}, safe=False)
+
             if not ExchangeValidationController().validate_direct_exchange(exchange_info["exchange_id"]).status:
                 ExchangeValidationController().cancel_exchange(direct_exchange)
                 return JsonResponse({"verified": False}, safe=False, status=403)
         
             token_seconds_elapsed = time.time() - exchange_info["exp"]
             if token_seconds_elapsed > VERIFY_EXCHANGE_TOKEN_EXPIRATION_SECONDS:
-                return JsonResponse({"verified": False}, safe=False, status=403)
+                return JsonResponse({"verified": False}, safe=False, status=400)
 
             with transaction.atomic():
                 participant = DirectExchangeParticipants.objects.filter(participant_nmec=request.user.username)
