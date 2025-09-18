a037dc422750f8bd0414cd91455796dba5e7197e
Process-ing <rbruno.p.o@gmail.com>
2025-09-18 13:26:14 +0100

    Make hasher robust to missing info

--- django/university/utils/ExchangeHasher.py
+++ django/university/utils/ExchangeHasher.py
@@ -6,7 +6,7 @@ class ExchangeHasher:
     def hash(exchanges: Iterable[Any], username: str) -> str:
         serialized_request = '|'.join(map(
             lambda exchange: ExchangeHasher.serialize_exchange(exchange, username),
-            sorted(exchanges, key=lambda x: x['courseUnitId'])  # sorted to ensure order between requests
+            sorted(exchanges, key=lambda x: x.get('courseUnitId', ''))  # sorted to ensure order between requests
         ))
 
         return hashlib.sha256(serialized_request.encode('utf8')).hexdigest()
@@ -16,9 +16,9 @@ class ExchangeHasher:
         # This function depends on the schema of the `CreateRequestData` type in the frontend
         return (
             username
-            + '+' + str(exchange['courseUnitId'])
-            + '+' + exchange['courseUnitName']
-            + '+' + exchange['classNameRequesterGoesFrom']
-            + '+' + exchange['classNameRequesterGoesTo']
-            + '+' + (str(exchange['other_student']['mecNumber']) if exchange['other_student'] else '')
+            + '+' + str(exchange.get('courseUnitId', ''))
+            + '+' + exchange.get('courseUnitName', '')
+            + '+' + exchange.get('classNameRequesterGoesFrom', '')
+            + '+' + exchange.get('classNameRequesterGoesTo', '')
+            + '+' + str((exchange['other_student'] or {}).get('mecNumber', ''))
         )
