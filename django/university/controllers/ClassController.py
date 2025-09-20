a812e61bdd0e87d00881b8eb13979a9e7db9c2b8
Tomás Palma <up202108880@edu.fe.up.pt>
2025-09-20 12:07:26 +0100

    refactor: added more prefetch related statements to get classes in class controller

--- django/university/controllers/ClassController.py
+++ django/university/controllers/ClassController.py
@@ -1,20 +1,20 @@
-from university.models import Class, Professor, Slot, SlotProfessor
+from university.models import Class, Professor, Slot, SlotProfessor, SlotClass
+from django.forms.models import model_to_dict
 
+from django.db.models import Prefetch
 
 class ClassController:
     @staticmethod
     def get_professors(slot):
-        slot_professors = SlotProfessor.objects.filter(slot_id=slot.id).values()
-        
-        professors = []
-        
-        for slot_professor in slot_professors:
-            professor = Professor.objects.get(id=slot_professor['professor_id'])
-            professors.append({
-                'id': professor.id,
-                'acronym': professor.professor_acronym,
-                'name': professor.professor_name
-            })
+        slot_professors = SlotProfessor.objects.filter(slot_id=slot.id).select_related("professor")
+
+        professors = [
+            {
+                'id': slot_professor.professor.id,
+                'acronym': slot_professor.professor.professor_acronym,
+                'name': slot_professor.professor.professor_name
+            } for slot_professor in slot_professors
+        ]
 
         return {
             'id': slot.id,
@@ -29,21 +29,24 @@ class ClassController:
 
     @staticmethod
     def get_classes(course_unit_id: int):
-        classes = Class.objects.filter(course_unit=course_unit_id).select_related(
-            'course_unit').prefetch_related('slotclass_set__slot').order_by("name")
+        classes = Class.objects.filter(
+            course_unit=course_unit_id
+        ).select_related(
+            'course_unit'
+        ).prefetch_related(
+            Prefetch('slotclass_set', queryset=SlotClass.objects.select_related('slot'))
+        ).order_by("name")
 
         result = []
-        for class_obj in classes:
-            slot_ids = [sc.slot_id for sc in class_obj.slotclass_set.all()]
-            slots = Slot.objects.filter(id__in=slot_ids)
 
-            slot_list = list(map(ClassController.get_professors, slots))
+        for class_obj in classes:
+            slot_list = [ClassController.get_professors(sc.slot) for sc in class_obj.slotclass_set.all()]
 
             result.append({
-                'id': class_obj.id,
-                'name': class_obj.name,
-                'vacancies': class_obj.vacancies,
-                'slots': slot_list
+                "id": class_obj.id,
+                "name": class_obj.name,
+                "vacancies": class_obj.vacancies,
+                "slots": slot_list
             })
 
         return result
