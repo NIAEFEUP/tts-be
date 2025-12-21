d580452105733038bddf4a3252bceca11584dbc8
Nunes <nunes.guicardoso@gmail.com>
2025-12-21 15:59:59 +0000

    fix: no more pointless requests to the db

--- django/university/routes/admin/AdminExchangeStatisticsView.py
+++ django/university/routes/admin/AdminExchangeStatisticsView.py
@@ -10,20 +10,18 @@ class AdminExchangeStatisticsView(APIView):
     def get(self, request):
 
 
-        #total requests
-        total_requests = DirectExchange.objects.count()
-        total_requests += MarketplaceExchange.objects.count()
-        total_requests += ExchangeUrgentRequests.objects.count()
+        direct_exchanges = DirectExchange.objects.all()
+        marketplace_exchanges = MarketplaceExchange.objects.all()
+        urgent_requests = ExchangeUrgentRequests.objects.all()
 
-        #accepted requests
-        accepted_count = DirectExchange.objects.filter(accepted=1).count()
-        accepted_count += MarketplaceExchange.objects.filter(accepted=1).count()
-        
-        #rejected requests
-        rejected_count = DirectExchange.objects.filter(accepted=0).count()
-        rejected_count += MarketplaceExchange.objects.filter(accepted=0).count()
+        total_requests = direct_exchanges.count() + marketplace_exchanges.count() + urgent_requests.count()
 
-        #pending requestsx
+        accepted_count = len(list(filter(lambda exchange: exchange.accepted == 1, direct_exchanges)))
+        accepted_count += len(list(filter(lambda exchange: exchange.accepted == 1, marketplace_exchanges)))
+
+        rejected_count = len(list(filter(lambda exchange: exchange.accepted == 0, direct_exchanges)))
+        rejected_count += len(list(filter(lambda exchange: exchange.accepted == 0, marketplace_exchanges)))
+
         pending_count = total_requests - (accepted_count + rejected_count)
 
 
