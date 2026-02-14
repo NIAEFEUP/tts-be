bdcca7fb26af4015ae4d4a42a8e24c6b304ba041
Tomás Palma <up202108880@edu.fe.up.pt>
2026-02-14 13:55:14 +0000

    fix: switched invalid static method call for instantiation

--- django/university/controllers/ClassController.py
+++ django/university/controllers/ClassController.py
@@ -113,7 +113,6 @@ class ClassController:
             course_unit_id = int(entry.get('ocorrencia_id'))
             start_time = float(entry.get('hora_inicio', 0)) / 3600.0
             duration = float(entry.get('aula_duracao', 0))
-            location = entry.get('sala_sigla')
             lesson_type = entry.get('tipo')
             day = schedule_controller.from_sigarra_day(entry.get('dia'))
 
@@ -127,7 +126,7 @@ class ClassController:
             slot = Slot(
                 id=entry.get('aula_id'),
                 lesson_type=entry.get('tipo'),
-                day=ScheduleController.from_sigarra_day(entry.get('dia'), 0),
+                day=day,
                 start_time=float(entry.get('hora_inicio', 0)) / 3600.0,
                 duration=float(entry.get('aula_duracao', 0)),
                 location=entry.get('sala_sigla'),
