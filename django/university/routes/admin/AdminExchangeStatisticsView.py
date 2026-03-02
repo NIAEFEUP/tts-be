866766bf9852b3972e24e6ac76cffc8cd9141ad5
Nuno <nunogonbraga@gmail.com>
2026-03-02 17:08:55 +0000

    improved statistics logic

--- django/university/routes/admin/AdminExchangeStatisticsView.py
+++ django/university/routes/admin/AdminExchangeStatisticsView.py
@@ -9,27 +9,35 @@ from exchange.models import ExchangeUrgentRequests
 class AdminExchangeStatisticsView(APIView):
     def get(self, request):
 
-
         direct_exchanges = DirectExchange.objects.all()
         marketplace_exchanges = MarketplaceExchange.objects.all()
         urgent_requests = ExchangeUrgentRequests.objects.all()
 
         total_requests = direct_exchanges.count() + marketplace_exchanges.count() + urgent_requests.count()
 
-        accepted_count = len(list(filter(lambda exchange: exchange.accepted == 1, direct_exchanges)))
-        accepted_count += len(list(filter(lambda exchange: exchange.accepted == 1, marketplace_exchanges)))
+        accepted_count = (
+            direct_exchanges.filter(accepted=True, admin_state='treated').count() +
+            marketplace_exchanges.filter(accepted=True, admin_state='treated').count() +
+            urgent_requests.filter(accepted=True, admin_state='treated').count()
+        )
 
-        rejected_count = len(list(filter(lambda exchange: exchange.accepted == 0, direct_exchanges)))
-        rejected_count += len(list(filter(lambda exchange: exchange.accepted == 0, marketplace_exchanges)))
+        rejected_count = (
+            direct_exchanges.filter(accepted=False, admin_state='treated').count() +
+            marketplace_exchanges.filter(accepted=False, admin_state='treated').count() +
+            urgent_requests.filter(accepted=False, admin_state='treated').count()
+        )
 
-        pending_count = total_requests - (accepted_count + rejected_count)
-
+        pending_count = (
+            direct_exchanges.filter(admin_state='untreated').count() +
+            marketplace_exchanges.filter(admin_state='untreated').count() +
+            urgent_requests.filter(admin_state='untreated').count()
+        )
 
         statistics = {
             'total_exchanges': total_requests,
             'accepted_exchanges': accepted_count,
             'rejected_exchanges': rejected_count,
-            'pending_exchanges': pending_count
+            'pending_exchanges': pending_count,
         }
 
         return JsonResponse(statistics, safe=False)
