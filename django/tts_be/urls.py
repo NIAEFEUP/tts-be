61582aac5930f4e8db95855abcf6313bfff6063d
Mariana Almeida <145381233+mariana2103@users.noreply.github.com>
2026-03-09 16:19:17 +0000

    Remove Sentry debug path from URLs

--- django/tts_be/urls.py
+++ django/tts_be/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
     path('', include('university.urls')),
 ]
