6bd631781ef91e9ede96d978b39e9f0a2ae6bf50
Nunes <nunes.guicardoso@gmail.com>
2026-04-20 07:11:48 +0100

    Feat/store participants slot (#300)
    * selected_slots field
    
    * update_schedule event handler

--- django/university/socket/participant.py
+++ django/university/socket/participant.py
@@ -1,23 +1,27 @@
 import uuid
 
 class Participant:
-    def __init__(self, sid, name):
+    def __init__(self, sid, name, selected_slots):
         self.sid = sid
         self.client_id = str(uuid.uuid4())
         self.name = name
+        self.selected_slots = selected_slots or []
         
     def to_json(self):
         return {
             'client_id': self.client_id,
             'name': self.name,
+            'selected_slots': self.selected_slots,
         }
         
     @staticmethod
     def from_json(sid, data):
         if 'name' not in data:
             raise ValueError('name is required')
-        return Participant(sid, data['name'])
+        return Participant(sid, data['name'], data.get('selected_slots', []))
     
     def update_from_json(self, data):
         if 'name' in data:
             self.name = data['name']
+        if 'selected_slots' in data:
+            self.selected_slots = data['selected_slots']
