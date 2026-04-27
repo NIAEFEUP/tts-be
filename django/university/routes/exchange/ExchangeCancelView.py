4dd6e3c59bc40881b60b04ae00001ac33d81d2c8
Mariana Almeida <marianacabralmeida@gmail.com>
2026-04-27 02:09:17 +0100

    fix: enhance permission checks for exchange operations and update URL authentication

--- django/university/routes/exchange/ExchangeCancelView.py
+++ django/university/routes/exchange/ExchangeCancelView.py
@@ -1,18 +1,34 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.views import View
 
-from university.controllers.AdminExchangeStateChangeController import AdminExchangeStateChangeController
 from university.controllers.ExchangeValidationController import ExchangeValidationController
 
-from exchange.models import DirectExchange, MarketplaceExchange
+from exchange.models import DirectExchange, DirectExchangeParticipants, ExchangeAdmin, MarketplaceExchange
 
 class ExchangeCancelView(View):
     def put(self, request, request_type, id):
-        
-        if request_type == "direct":
-            ExchangeValidationController().cancel_exchange(DirectExchange.objects.get(id=id))
+        username = request.user.username
+        is_admin = ExchangeAdmin.objects.filter(username=username).exists()
 
-        if request_type == "marketplace":
-            ExchangeValidationController().cancel_exchange(MarketplaceExchange.objects.get(id=id))
+        if request_type == "direct":
+            try:
+                exchange = DirectExchange.objects.get(id=id)
+            except DirectExchange.DoesNotExist:
+                return HttpResponse(status=404)
+            is_participant = DirectExchangeParticipants.objects.filter(
+                direct_exchange=exchange, participant_nmec=username
+            ).exists()
+            if not is_admin and not is_participant:
+                return HttpResponse(status=403)
+            ExchangeValidationController().cancel_exchange(exchange)
+
+        elif request_type == "marketplace":
+            try:
+                exchange = MarketplaceExchange.objects.get(id=id)
+            except MarketplaceExchange.DoesNotExist:
+                return HttpResponse(status=404)
+            if not is_admin and exchange.issuer_nmec != username:
+                return HttpResponse(status=403)
+            ExchangeValidationController().cancel_exchange(exchange)
 
         return HttpResponse(status=200)
